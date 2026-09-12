d9fb14166fbcf3b07f7082fbecbd8f58a1ef0e51
kfemelue <kfemelue@gmail.com>
2026-09-12 10:33:02 -0500

    Review JS basics and DOM functions

---
+++ script.js
@@ -0,0 +1,54 @@
+// Variables
+let message = "Hello World";
+
+console.log(message)
+
+message = "Hello 202608 Cohort!"
+
+console.log(message)
+
+var myNumber = 34567;
+
+console.log(myNumber);
+
+myNumber= 67890;
+
+console.log(myNumber);
+
+// Operators
+
+// Arithmetic: + - / *
+
+
+console.log(message + " Today is Saturday");
+let myName = "Dili"
+console.log(`Hello, my name is ${myName}, I am 29 years old`);
+console.log("Hello, my name is " +  myName + " I am 29 years old");
+
+// Comparison
+
+let x = 5 < 9 || 2 > 3;
+
+let y = 5 !== "5"
+
+// AND --> &&
+// OR --> ||
+// NOT !
+
+console.log(y)
+
+// document.querySelector("") selects the first element that matches the qeury
+let myHeader = document.querySelector("h1")
+
+console.log(myHeader)
+myHeader.innerText = "Hello World"
+myHeader.style = "color: blue;"
+console.log( document.querySelectorAll("p"))
+
+// document.getElementById("") selects the unique element that matches the id input
+console.log(document.getElementById("p2"))
+
+
+
+
+
